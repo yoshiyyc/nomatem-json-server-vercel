310efe34b1e40b7922e6ae054f703df13df3aa91
Yoshi Y <95995009+yoshiyyc@users.noreply.github.com>
2022-12-21 16:01:27 +0800

    Added auth rules

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // See https://github.com/typicode/json-server#module
 const cors = require('cors');
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 const auth = require("json-server-auth");
 const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
+
+const rules = auth.rewriter({
+  // Permission rules
+  posts: 644,
+  comments: 644,
+  friends: 644,
+  users: 600,
+})
+
 server.use(cors())
 server.use(middlewares)
 server.db = router.db;
+server.use(rules);
 server.use(auth);
 server.use(router)
 server.listen(3000, () => {
